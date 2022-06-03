9d1284bf69879ee121ae7ae5b40ba7a23733e321
Duy303909 <dannytran010@gmail.com>
2022-06-03 11:46:07 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Í chào m ! 🖐🖐🖐',
+    introDesc: `T có điều muốn hỏi m , m nhớ phải trả lời thật lòng nha :3`,
+    btnIntro: 'OK',
+    title: 'M thích t không ? 😙',
+    desc: 'M mà không trả lời mà thoát ra là làm ny t đó :> ',
+    btnYes: 'Thích :3',
+    btnNo: 'Dell nha :)) ',
+    question:'Nói cho t biết vì sao m thích t hi vọng ko phải do cái chữ "Dell nha" chạy 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì m đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Cảm ơn, T cũng thích m lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho t nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100048030812321'
+}
